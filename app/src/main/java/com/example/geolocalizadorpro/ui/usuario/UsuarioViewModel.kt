6b7fbfdaff5de3d34ad888922faa57842217a208
LeoNardoz333 <leo.vel330@gmail.com>
2023-09-25 19:43:06 -0600

    Pequeño avance

--- app/src/main/java/com/example/geolocalizadorpro/ui/usuario/UsuarioViewModel.kt
+++ app/src/main/java/com/example/geolocalizadorpro/ui/usuario/UsuarioViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.geolocalizadorpro.ui.home
+package com.example.geolocalizadorpro.ui.usuario
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class UsuarioViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"

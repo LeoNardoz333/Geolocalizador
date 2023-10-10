e06cf383feed35f2cce0beeecc040e0c3cacd211
LeoNardoz333 <leo.vel330@gmail.com>
2023-10-09 19:34:27 -0600

    Se terminó el diseño del menú de administradores

--- app/src/main/java/com/example/geolocalizadorpro/Menu_admins.kt
+++ app/src/main/java/com/example/geolocalizadorpro/Menu_admins.kt
@@ -2,10 +2,18 @@ package com.example.geolocalizadorpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Menu_admins : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_admins)
+
+        Regresar()
+    }
+    fun Regresar(){
+        val regresar: FloatingActionButton = findViewById(R.id.btnRegresar)
+        regresar.setOnClickListener {
+        }
     }
 }
